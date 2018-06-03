3040084fac4fa08b6906abc786328d58bd567a71
Tahara <tahara@studiom-web.net>
2018-06-03 15:37:16 +0900

    JSXについて 2

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//      return <h1>Hello world!</h1>;
+//   }
+// }
 class App extends Component {
   render() {
-     return <h1>Hello world!</h1>;
+     return React.createElement(
+       "div",
+       null,
+       "Hello world!"
+     )
   }
 }
 
